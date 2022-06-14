0649d3acdbaaf9194fd1aa1841f7f72cd24b9a2c
tranqhuy46 <tranquochuy4698@gmail.com>
2022-06-14 19:55:27 +0700

    opt: setup with type script

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import type { DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=optinal"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
