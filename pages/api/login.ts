d78be211d8b8a44d4a72b185f851b5e72a95a4d5
tranqhuy46 <tranquochuy4698@gmail.com>
2022-06-15 16:50:52 +0700

    feat: imple login API

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,37 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import passport from "passport";
 import nextConnect from "next-connect";
-import type { IAccount } from "../../shared/models/Account";
-import { localStrategy } from "../../lib/passport-local";
-import { setLoginSession } from "../../lib/auth";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { sessionOptions } from "../../lib/session";
+import { IAccount } from "../../shared/models/Account";
+import AccountSchema from "../../shared/schema/Account.schema";
+import dbMiddleware from "./middleware/db";
 
-const authenticate = (
-  method: string,
-  req: NextApiRequest,
-  res: NextApiResponse
-) =>
-  new Promise<IAccount>((resolve, reject) => {
-    passport.authenticate(method, { session: false }, (error, account) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(account);
+const handler = nextConnect();
+
+handler.use(dbMiddleware);
+
+async function loginController(req: NextApiRequest, res: NextApiResponse) {
+  const { email, password }: { email: string; password: string } =
+    await req.body;
+  try {
+    const data = await AccountSchema.findOne({ email });
+    if (data) {
+      // validate password
+      if (data.password !== password) {
+        res.status(403).json({ message: "invalid password" });
+        return;
       }
-    })(req, res);
-  });
+      //
 
-passport.use(localStrategy);
+      const user: Partial<IAccount> = { email: data.email };
 
-export default nextConnect<NextApiRequest, NextApiResponse>()
-  .use(passport.initialize())
-  .post(async (req, res) => {
-    try {
-      const user = await authenticate("local", req, res);
-      const session = { ...user };
-      await setLoginSession(res, session);
-      res.status(200).send({ done: true });
-    } catch (error: any) {
-      console.error(error);
-      res.status(401).send(error.message);
+      req.session.user = user;
+
+      await req.session.save();
+
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "user not found" });
     }
-  });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
+
+export default withIronSessionApiRoute(
+  handler.post(loginController),
+  sessionOptions
+);
